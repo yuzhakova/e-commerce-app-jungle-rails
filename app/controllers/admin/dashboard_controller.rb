a2f443f5c73fc8fb801293058ebab0974518504f
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-20 18:22:32 +0000

    update admin dashboard display prodcts within categories count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   
   def show
+    @product_count = Product.all.count
+    @apparel_count = Product.where(category_id: 1).count
+    @electronic_count = Product.where(category_id: 2).count
+    @furniture_count = Product.where(category_id: 3).count
   end
 end
