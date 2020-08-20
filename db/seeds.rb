b5f76733e48e0e6a0f92536a2b649222dcc41b5f
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-20 05:18:53 +0000

    add sold-out badge for products with 0 inventory quantity on hand

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,8 @@ cat1.products.create!({
   name:  'Men\'s Classy shirt',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel1.jpg'),
-  quantity: 10,
+  quantity: 0,
+  # quantity: 10,
   price: 64.99
 })
 
