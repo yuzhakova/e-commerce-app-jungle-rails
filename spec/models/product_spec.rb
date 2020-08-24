2c36b9b4d126a5ce57616cb068029e7de6f0f0a1
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-24 23:03:05 +0000

    add specs for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
+    # :name, presence: true
+    # :price, presence: true
+    # :quantity, presence: true
+    # :category, presence: true
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, :type => :model do
+  describe 'Validations' do
+    it "is valid with valid attributes of name, price, quantity and category" do
+      @category = Category.new
+      @category.id = 6
+      @category.name = 'test'
+      
+      @product = Product.new
+      @product.name = "prod"
+      @product.price = 10000
+      @product.quantity = 10000
+      @product.category_id = @category
+
+      @category.products = [@product]
+      
+      expect(@product).to be_valid
+    end
+
+    it "is not valid without a name" do
+      @category = Category.new
+      @category.id = 6
+      @category.name = 'test'
+
+      @product = Product.new
+      @product.name = nil
+      @product.price = 10000
+      @product.quantity = 10000
+      @product.category_id = @category
+
+      @category.products = [@product]
+      
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      @category = Category.new
+      @category.id = 6
+      @category.name = 'test'
+
+      @product = Product.new
+      @product.name = 'test'
+      @product.quantity = 1
+      @product.price = nil
+      @product.category_id = @category
+
+      @category.products = [@product]
+
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      @category = Category.new
+      @category.id = 6
+      @category.name = 'test'
+
+      @product = Product.new
+      @product.name = 'test'
+      @product.price = 1
+      @product.quantity = nil
+      @product.category_id = @category
+
+      @category.products = [@product]
+
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      @category = Category.new
+      @category.id = 6
+      @category.name = 'test'
+
+      @product = Product.new
+      @product.name = 'test'
+      @product.price = 1
+      @product.quantity = 1
+      @product.category_id = nil
+
+      @category = [@product]
+
+      expect(@product).to_not be_valid
+    end
+  end
 end
