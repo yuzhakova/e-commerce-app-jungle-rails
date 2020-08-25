9d74fd2b3ef242116b562d2002b48b984b4fa839
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-25 00:40:49 +0000

    add tests to validate all fields completion

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    context 'checking for content in fields' do
+
+      it "is valid with valid attributes of first/last name, password/password_confirmation and email" do
+        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test')
+
+        expect(@user).to be_valid
+      end
+
+      it "is invalid with first name as nil" do
+        @user = User.create(first_name: nil, last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test')
+
+        expect(@user.errors.full_messages.length == 1)
+        expect(@user.errors.full_messages[0] == "First_name can't be blank")
+        expect(@user).to_not be_valid
+      end
+
+      it "is invalid with last name as nil" do
+        @user = User.create(first_name: 'test', last_name: nil, password: 'test', password_confirmation: 'test', email: 'test')
+
+        expect(@user.errors.full_messages.length == 1)
+        expect(@user.errors.full_messages[0] == "Last_name can't be blank")
+        expect(@user).to_not be_valid
+      end
+
+      it "is invalid with no password" do
+        @user = User.create(first_name: 'test', last_name: 'test', password: nil, password_confirmation: 'test', email: 'test')
+
+        expect(@user.errors.full_messages.length == 1)
+        expect(@user.errors.full_messages[0] == "password can't be blank")
+        expect(@user).to_not be_valid
+      end
+
+      it "is invalid with no password confirmation" do
+        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: nil, email: 'test')
+
+        expect(@user.errors.full_messages.length == 1)
+        expect(@user.errors.full_messages[0] == "Password_confirmation can't be blank")
+        expect(@user).to_not be_valid
+      end
+      
+      it "is invalid with no email" do
+        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: nil)
+
+        expect(@user.errors.full_messages.length == 1)
+        expect(@user.errors.full_messages[0] == "email can't be blank")
+        expect(@user).to_not be_valid
+      end
+
+    end
+    # emails must be unique and not case sensitive
+    #pw and pw_conf don't match
+  end
 end
