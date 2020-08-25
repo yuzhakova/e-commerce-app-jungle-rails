5060c4c7d9971b9392351f3ce2dfb49df4826cbf
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-25 22:46:09 +0000

    add confirmation order

--- spec/mailers/previews/order_mailer_preview.rb
+++ spec/mailers/previews/order_mailer_preview.rb
@@ -2,7 +2,15 @@
 class OrderMailerPreview < ActionMailer::Preview
 
   def send_email
+    product = Product.new(name: 'product', description: "some kind of product", price_cents: 1000, quantity: 100, category_id: 1, image: "https://miro.medium.com/max/5760/1*2bjwCLaA8TfH40OXcyLNvA.png")
+    quantity = 12
     order = Order.create(total_cents: 1000, stripe_charge_id: 2, email: 'bob@bob')
+    order.line_items.new(
+      product: product,
+      quantity: quantity,
+      item_price: product.price,
+      total_price: product.price * quantity
+    )
     # puts order.email
     OrderMailer.send_email(order)
   end
