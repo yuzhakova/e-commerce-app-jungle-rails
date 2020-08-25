41e28f077939950355b544c870e0ce062ec36fbf
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-25 00:53:41 +0000

    additional refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true
-  validates :email, presence: true, uniqueness: { case_sensistive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,68 +10,76 @@ RSpec.describe User, type: :model do
       end
 
       it "is invalid with first name as nil" do
-        @user = User.create(first_name: nil, last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test')
-        print @user.errors.full_messages
+        @user = User.create(first_name: nil, last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test1')
         expect(@user.errors.full_messages.length).to eql(1)
 
-        expect(@user.errors.full_messages[0] == "First_name can't be blank")
+        expect(@user.errors.full_messages[0]).to eql("First name can't be blank")
         expect(@user).to_not be_valid
       end
 
       it "is invalid with last name as nil" do
-        @user = User.create(first_name: 'test', last_name: nil, password: 'test', password_confirmation: 'test', email: 'test')
+        @user = User.create(first_name: 'test', last_name: nil, password: 'test', password_confirmation: 'test', email: 'test2')
 
-        expect(@user.errors.full_messages.length == 1)
-        expect(@user.errors.full_messages[0] == "Last_name can't be blank")
+        expect(@user.errors.full_messages.length).to eql(1)
+        expect(@user.errors.full_messages[0]).to eql("Last name can't be blank")
         expect(@user).to_not be_valid
       end
 
       it "is invalid with no password" do
-        @user = User.create(first_name: 'test', last_name: 'test', password: nil, password_confirmation: 'test', email: 'test')
-
-        expect(@user.errors.full_messages.length == 1)
-        expect(@user.errors.full_messages[0] == "password can't be blank")
+        @user = User.create(first_name: 'test', last_name: 'test', password: nil, password_confirmation: 'test', email: 'test3')
+        expect(@user.errors.full_messages.length).to eql(2)
+        expect(@user.errors.full_messages[0]).to eql("Password can't be blank")
+        expect(@user.errors.full_messages[1]).to eql("Password can't be blank")
         expect(@user).to_not be_valid
       end
 
       it "is invalid with no password confirmation" do
-        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: nil, email: 'test')
+        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: nil, email: 'test4')
 
-        expect(@user.errors.full_messages.length == 1)
-        expect(@user.errors.full_messages[0] == "Password_confirmation can't be blank")
+        expect(@user.errors.full_messages.length).to eql(1)
+        expect(@user.errors.full_messages[0]).to eql("Password confirmation can't be blank")
         expect(@user).to_not be_valid
       end
       
       it "is invalid with no email" do
         @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: nil)
 
-        expect(@user.errors.full_messages.length == 1)
-        expect(@user.errors.full_messages[0] == "email can't be blank")
+        expect(@user.errors.full_messages.length).to eql(1)
+        expect(@user.errors.full_messages[0]).to eql("Email can't be blank")
         expect(@user).to_not be_valid
       end
 
-
-
     context "verifying email inputs" do
-      it "is invalid if email is already taken" do
-        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test')
-        @user2 = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test')
+      it "is invalid if email is already taken: case sensitive" do
+        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test3')
+        @user2 = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test3')
 
-        expect(@user2.errors.full_messages.length == 1)
-        expect(@user2.errors.full_messages[0] == "Email has already been taken")
+        expect(@user2.errors.full_messages.length).to eql(1)
+        expect(@user2.errors.full_messages[0]).to eql("Email has already been taken")
         expect(@user2).to_not be_valid
+
+        expect(@user.errors.full_messages.length).to eql(0)
+        expect(@user).to be_valid
       end
 
-      it "is invalid if email is already taken not case sensitive" do
+      it "is invalid if email is already taken: not case sensitive" do
+        @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: 'test4')
+        @user2 = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'test', email: 'TeSt4')
       
+        expect(@user2.errors.full_messages.length).to eql(1)
+        expect(@user2.errors.full_messages[0]).to eql("Email has already been taken")
+        expect(@user2).to_not be_valid
+
+        expect(@user.errors.full_messages.length).to eql(0)
+        expect(@user).to be_valid
       end
     end
 
     it "is invalid if password and password confirmation do not match" do
-      @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'tEsT', email: 'test')
+      @user = User.create(first_name: 'test', last_name: 'test', password: 'test', password_confirmation: 'tEsT', email: 'test2')
 
-      expect(@user.errors.full_messages.length == 1)
-      expect(@user.errors.full_messages[0] == "Password Confirmation doesn't match Password")
+      expect(@user.errors.full_messages.length).to eql(1)
+      expect(@user.errors.full_messages[0]).to eql("Password confirmation doesn't match Password")
       expect(@user).to_not be_valid
     end
   end
