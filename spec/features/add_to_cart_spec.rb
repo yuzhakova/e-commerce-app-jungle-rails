b5dd6b314e7a9a2ec4783889ec3260d33cec1fe2
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-25 22:11:37 +0000

    add test click and cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+ RSpec.feature "User can add to cart", type: :feature, js: true do
+   before :each do
+     @category = Category.create! name: 'Apparel'
+
+     10.times do |n|
+       @category.products.create!(
+         name: Faker::Hipster.sentence(3),
+         description: Faker::Hipster.paragraph(4),
+         image: open_asset('apparel1.jpg'),
+         quantity: 10,
+         price: 64.99
+       )
+     end
+   end
+
+
+ end
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # Click on a product partial
+  # Test if I make it to that partial page
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name: Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    # puts page.html
+    page.find('.product').find('header').click_link
+
+    # DEBUG / VERIFY
+
+    expect(page).to have_css '.products-show'
+    save_screenshot    
+  end
+end
